c68e1e244545731d1b9f65bee4aa5efdd33f6638
Gustavo Duran <gustavoduran22@gmail.com>
2022-06-10 11:40:57 -0300

    adicioanando informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('cypress')
 
+projectId: "fh4ost"
+
 module.exports = defineConfig({
+  projectId: "fh4ost",
   viewportWidth: 1440,
   viewportHeight: 900,
+  
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -11,4 +15,5 @@ module.exports = defineConfig({
     },
     baseUrl: 'https://buger-eats-qa.vercel.app',
   },
+  
 })
